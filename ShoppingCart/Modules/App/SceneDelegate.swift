d4316637ed6d19d7380fb0bce878c4d2211089b9
rabin.pun <rabin.pun@ebpearls.com>
2022-08-19 08:00:11 +0545

    setup inital view in window

--- ShoppingCart/Modules/App/SceneDelegate.swift
+++ ShoppingCart/Modules/App/SceneDelegate.swift
@@ -11,12 +11,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    private lazy var database: StorageProvider = {
+        Database(modalName: "ShoppingCart")
+    }()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = scene as? UIWindowScene else { return }
+        setupInitalView(windowScens: windowScene)
+    }
+    
+    private func setupInitalView (windowScens: UIWindowScene) {
+        window = UIWindow(windowScene: windowScens)
+        window!.rootViewController = CartListViewBuilder(database: database).buildWithNavigationController()
+        window!.makeKeyAndVisible()
     }
 
 }
