49bb30e14120048a02f81a5f3e97ef1de9146fcf
rabin.pun <rabin.pun@ebpearls.com>
2022-08-21 12:27:53 +0545

    add item creating functionality and show error alerts

--- ShoppingCart/Modules/AddCartItem/AddItemController.swift
+++ ShoppingCart/Modules/AddCartItem/AddItemController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 
 enum TextFieldType: CaseIterable {
     case name, price, quantity, tax
@@ -41,25 +42,35 @@ enum TextFieldType: CaseIterable {
 
 final class AddItemController: UIViewController {
     
+    ///Constraints
+    private let buttonHeight: CGFloat = 40
+    
     var presenter: AddItemPresentable!
     
     private let textFields = TextFieldType.allCases
     
-    lazy var textFieldStackView: UIStackView = {
+    private lazy var textFieldStackView: UIStackView = {
         let stackView = UIFactory.stackView(axis: .vertical)
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
+    private lazy var createButton = UIFactory.textButton(text: "Create item", cornerRadius: buttonHeight * 0.25)
+    
+    private var alertCancellable: AnyCancellable?
+    
     override func viewDidLoad() {
         view.backgroundColor = .systemTeal
         
-        addTextFields()
+        addViews()
+        createButton.addTarget(self, action: #selector(createButtonClicked), for: .touchUpInside)
     }
     
-    private func addTextFields() {
+    private func addViews() {
         
         view.addSubview(textFieldStackView)
+        view.addSubview(createButton)
+        
         textFields.forEach { textField in
             let stackView = UIFactory.stackView(axis: .vertical, spacing: 10)
             let label = UIFactory.label(font: .systemFont(ofSize: .FontSize.regular.value, weight: .semibold),text: textField.title)
@@ -76,16 +87,35 @@ final class AddItemController: UIViewController {
             textFieldStackView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
             textFieldStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
             textFieldStackView.heightAnchor.constraint(equalToConstant: 300),
+            
+            createButton.topAnchor.constraint(equalTo: textFieldStackView.bottomAnchor, constant: 30),
+            createButton.centerXAnchor.constraint(equalTo: textFieldStackView.centerXAnchor),
+            createButton.heightAnchor.constraint(equalToConstant: buttonHeight),
+            createButton.widthAnchor.constraint(equalToConstant: 120),
                 
         ])
         
+    }
+    
+    @objc private func createButtonClicked() {
+        let stackViews = textFieldStackView.arrangedSubviews as! [UIStackView]
+        let textFields = stackViews.reduce([Textfield](), { $0 + $1.arrangedSubviews.filter({ $0 is Textfield }) as! [Textfield] })
+        let parameters: [(ParameterType,String)] = [
+            (.name, textFields[0].text ?? ""),
+            (.price, textFields[1].text ?? ""),
+            (.quantity, textFields[2].text ?? ""),
+            (.tax, textFields[3].text ?? ""),
+        ]
+        presenter.addItem(parameters: parameters)
     }
     
 }
 
 extension AddItemController: AddItemPresenterDelegate {
     func showAlert(title: String, message: String, alertActions: [AlertAction]) {
-    
+        alertCancellable = alert(title: title, msg: message, actions: alertActions).sink { alert in
+            alert.actionClosure?()
+        }
     }
     
 }
