93a6b8589d111d237e70a85befb92e32a4afd91a
rabin.pun <rabin.pun@ebpearls.com>
2022-08-21 12:27:16 +0545

    handle item add error

--- ShoppingCart/Modules/AddCartItem/AddItemPresenter.swift
+++ ShoppingCart/Modules/AddCartItem/AddItemPresenter.swift
@@ -15,8 +15,25 @@ protocol AddItemPresenterDelegate: UIViewController {
 /// Protocol for AddItem presenter
 protocol AddItemPresentable {
     var delegate: AddItemPresenterDelegate? { get set }
+    
+    func addItem(parameters: [(ParameterType,String)])
+}
 
-    func addItem(name: String, image: String?, tax: Float, quantity: Int16, price: Float)
+enum ParameterType: String {
+    case name, tax, price, quantity, image
+}
+
+enum AddItemError: LocalizedError {
+    case empty(ParameterType), invalid(ParameterType)
+    
+    var errorDescription: String? {
+        switch self {
+        case .empty(let parameterType):
+            return "\(parameterType.rawValue) is empty."
+        case .invalid(let parameterType):
+            return "\(parameterType.rawValue) is invalid."
+        }
+    }
 }
 
 final class AddItemPresenter: AddItemPresentable {
@@ -31,8 +48,40 @@ final class AddItemPresenter: AddItemPresentable {
         self.router = router
     }
     
-    func addItem(name: String, image: String?, tax: Float, quantity: Int16, price: Float) {
-        addCartItemUseCase.create(CartItem.Object(id: UUID().uuidString, name: name, image: image, tax: tax, quantity: quantity, price: price, updatedAt: Date()))
+    func addItem(parameters: [(ParameterType,String)]) {
+        do {
+            try createItem(parameters)
+        } catch {
+            delegate?.showAlert(title: "Error", message: error.localizedDescription, alertActions: [AlertAction.ok(nil)])
+        }
+    }
+    
+    private func createItem(_ parameters: [(ParameterType,String)]) throws {
+        for parameter in parameters {
+            if parameter.1.isEmpty {
+                throw AddItemError.empty(ParameterType(rawValue: parameter.0.rawValue)!)
+            } else {
+                switch parameter.0 {
+                case .tax:
+                    if Float(parameter.1) == nil {
+                        throw AddItemError.invalid(ParameterType(rawValue: parameter.0.rawValue)!)
+                    }
+                case .price:
+                    if Float(parameter.1) == nil {
+                        throw AddItemError.invalid(ParameterType(rawValue: parameter.0.rawValue)!)
+                    }
+                case .quantity:
+                    if Int16(parameter.1) == nil {
+                        throw AddItemError.invalid(ParameterType(rawValue: parameter.0.rawValue)!)
+                    }
+                default:
+                    break
+                }
+            }
+        }
+        
+        addCartItemUseCase.create(CartItem.Object(id: UUID().uuidString, name: parameters[0].1, image: nil, tax: Float(parameters[3].1)!, quantity: Int16(parameters[2].1)!, price: Float(parameters[1].1)!, updatedAt: Date()))
+        router.dismissView()
     }
     
 }
